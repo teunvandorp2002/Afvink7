a712b9a7a8e1f7ac595d45506e566ce4380841e4
teunvandorp2002 <teunvandorp2002@users.noreply.github.com>
2020-10-30 14:56:17 +0100

    JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"biotools.bioseq","l":"DNA"},{"p":"biotools.bioseq","l":"Protein"},{"p":"biotools.bioseq","l":"RNA"},{"p":"biotools.bioapp","l":"SequenceVisualiser"},{"p":"<Unnamed>","l":"Start"}]
